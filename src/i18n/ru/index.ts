e71b8b95c228ff5cbdee669004504231866fcf28
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2025-01-27 15:40:34 -0800

    crowdin[bot]: New translations for index.ts (Russian)

--- src/i18n/ru/index.ts
+++ src/i18n/ru/index.ts
@@ -1,69 +1,67 @@
-import type { Translation } from '../i18n-types.js';
-
-export default {
-	GUARDS: {
-		DISABLED_COMMAND: 'Эта команда на данный момент не доступна.',
-		MAINTENANCE: 'Бот закрыт на техническое обслуживание.',
-		GUILD_ONLY: 'Эту команду можно использовать только на сервере.',
-		NSFW: 'Эта команда доступна только в чатах 18+.',
-	},
-	ERRORS: {
-		UNKNOWN: 'Произошла непонятная ошибка.',
-	},
-	SHARED: {
-		NO_COMMAND_DESCRIPTION: 'Описание отсутствует.',
-	},
-	COMMANDS: {
-		INVITE: {
-			DESCRIPTION: 'Пригласить бота на свой сервер!',
-			EMBED: {
-				TITLE: 'Хочешь видеть меня у себя на сервере?',
-				DESCRIPTION: '[Жми здесь]({link}) чтобы добавить бота!',
-			},
-		},
-		PREFIX: {
-			NAME: 'prefix',
-			DESCRIPTION: 'Изменить префикс для бота.',
-			OPTIONS: {
-				PREFIX: {
-					NAME: 'new_prefix',
-					DESCRIPTION: 'Новый префикс для бота.',
-				},
-			},
-			EMBED: {
-				DESCRIPTION: 'Префикс бота изменен на `{prefix}`.',
-			},
-		},
-		MAINTENANCE: {
-			DESCRIPTION: 'Установить режим технического обслуживания бота.',
-			EMBED: {
-				DESCRIPTION: 'Режим Технического Обслуживания установлен на `{state}`.',
-			},
-		},
-		STATS: {
-			DESCRIPTION: 'Получить статистику по боту.',
-			HEADERS: {
-				COMMANDS: 'Команды',
-				GUILDS: 'Сервера',
-				ACTIVE_USERS: 'Активные пользователи',
-				USERS: 'Пользователи',
-			},
-		},
-		HELP: {
-			DESCRIPTION: 'Глобальная справка по боту и его командам',
-			EMBED: {
-				TITLE: 'Панель помощи',
-				CATEGORY_TITLE: '{category} команды',
-			},
-			SELECT_MENU: {
-				TITLE: 'Выбери категорию',
-				CATEGORY_DESCRIPTION: '{category} команды',
-			},
-		},
-		PING: {
-			DESCRIPTION: 'Тук-тук!',
-			MESSAGE:
-				'{member} Что нужно? Было потрачено {time} милисекунд на генерацию ответа. {heartbeat}',
-		},
-	},
-} as Translation;
+import type { BaseTranslation } from '../i18n-types.js';
+export default ({
+  GUARDS: {
+    DISABLED_COMMAND: "Эта команда на данный момент не доступна.",
+    MAINTENANCE: "Бот закрыт на техническое обслуживание.",
+    GUILD_ONLY: "Эту команду можно использовать только на сервере.",
+    NSFW: "Эта команда доступна только в чатах 18+."
+  },
+  ERRORS: {
+    UNKNOWN: "Произошла непонятная ошибка."
+  },
+  SHARED: {
+    NO_COMMAND_DESCRIPTION: 'No description provided.'
+  },
+  COMMANDS: {
+    INVITE: {
+      DESCRIPTION: "Пригласить бота на свой сервер!",
+      EMBED: {
+        TITLE: "Хочешь видеть меня у себя на сервере?",
+        DESCRIPTION: '[Click here]({link}) to invite me!'
+      }
+    },
+    PREFIX: {
+      NAME: 'prefix',
+      DESCRIPTION: 'Change the prefix of the bot.',
+      OPTIONS: {
+        PREFIX: {
+          NAME: 'new_prefix',
+          DESCRIPTION: 'The new prefix of the bot.'
+        }
+      },
+      EMBED: {
+        DESCRIPTION: 'Prefix changed to `{prefix:string}`.'
+      }
+    },
+    MAINTENANCE: {
+      DESCRIPTION: 'Set the maintenance mode of the bot.',
+      EMBED: {
+        DESCRIPTION: 'Maintenance mode set to `{state:string}`.'
+      }
+    },
+    STATS: {
+      DESCRIPTION: 'Get some stats about the bot.',
+      HEADERS: {
+        COMMANDS: 'Commands',
+        GUILDS: 'Guild',
+        ACTIVE_USERS: 'Active Users',
+        USERS: 'Users'
+      }
+    },
+    HELP: {
+      DESCRIPTION: 'Get global help about the bot and its commands',
+      EMBED: {
+        TITLE: 'Help panel',
+        CATEGORY_TITLE: '{category:string} Commands'
+      },
+      SELECT_MENU: {
+        TITLE: 'Select a category',
+        CATEGORY_DESCRIPTION: '{category:string} commands'
+      }
+    },
+    PING: {
+      DESCRIPTION: 'Pong!',
+      MESSAGE: '{member:string} Pong! The message round-trip took {time:number}ms.{heartbeat:string}'
+    }
+  }
+} as BaseTranslation);
