768b2e8c700e3bce1fcea194c7b0e85df501cba0
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2025-01-27 15:40:17 -0800

    crowdin[bot]: New translations for index.ts (French)

--- src/i18n/fr/index.ts
+++ src/i18n/fr/index.ts
@@ -1,69 +1,67 @@
-import type { Translation } from '../i18n-types.js';
-
-export default {
-	GUARDS: {
-		DISABLED_COMMAND: 'Cette commande est désactivée.',
-		MAINTENANCE: 'Ce bot est en mode maintenance.',
-		GUILD_ONLY: "Cette commande ne peut être utilisée qu'en serveur.",
-		NSFW: 'Cette commande ne peut être utilisée que dans un salon NSFW.',
-	},
-	ERRORS: {
-		UNKNOWN: 'Une erreur est survenue.',
-	},
-	SHARED: {
-		NO_COMMAND_DESCRIPTION: 'Aucune description fournie.',
-	},
-	COMMANDS: {
-		INVITE: {
-			DESCRIPTION: 'Invitez le bot sur votre serveur!',
-			EMBED: {
-				TITLE: 'Invite moi sur ton serveur!',
-				DESCRIPTION: "[Clique ici]({link}) pour m'inviter!",
-			},
-		},
-		PREFIX: {
-			NAME: 'prefixe',
-			DESCRIPTION: 'Change le préfix du bot.',
-			OPTIONS: {
-				PREFIX: {
-					NAME: 'nouveau_prefix',
-					DESCRIPTION: 'Le nouveau préfix du bot.',
-				},
-			},
-			EMBED: {
-				DESCRIPTION: 'Prefix changé en `{prefix}`.',
-			},
-		},
-		MAINTENANCE: {
-			DESCRIPTION: 'Met le mode maintenance du bot.',
-			EMBED: {
-				DESCRIPTION: 'Le mode maintenance a été définie à `{state}`.',
-			},
-		},
-		STATS: {
-			DESCRIPTION: 'Obtiens des statistiques sur le bot.',
-			HEADERS: {
-				COMMANDS: 'Commandes',
-				GUILDS: 'Serveurs',
-				ACTIVE_USERS: 'Utilisateurs actifs',
-				USERS: 'Utilisateurs',
-			},
-		},
-		HELP: {
-			DESCRIPTION: "Obtenez de l'aide globale sur le bot et ses commandes",
-			EMBED: {
-				TITLE: "Pannel d'aide",
-				CATEGORY_TITLE: 'Commandes de {category}',
-			},
-			SELECT_MENU: {
-				TITLE: 'Sélectionnez une catégorie',
-				CATEGORY_DESCRIPTION: 'Commandes de {category}',
-			},
-		},
-		PING: {
-			DESCRIPTION: 'Pong!',
-			MESSAGE:
-				'{member} Pong! Le temps de réponse de la réponse était {time}ms.{heartbeat}',
-		},
-	},
-} as Translation;
+import type { BaseTranslation } from '../i18n-types.js';
+export default ({
+  GUARDS: {
+    DISABLED_COMMAND: 'This command is currently disabled.',
+    MAINTENANCE: 'This bot is currently in maintenance mode.',
+    GUILD_ONLY: 'This command can only be used in a server.',
+    NSFW: 'This command can only be used in a NSFW channel.'
+  },
+  ERRORS: {
+    UNKNOWN: 'An unknown error occurred.'
+  },
+  SHARED: {
+    NO_COMMAND_DESCRIPTION: 'No description provided.'
+  },
+  COMMANDS: {
+    INVITE: {
+      DESCRIPTION: 'Invite the bot to your server!',
+      EMBED: {
+        TITLE: 'Invite me on your server!',
+        DESCRIPTION: '[Click here]({link}) to invite me!'
+      }
+    },
+    PREFIX: {
+      NAME: 'prefix',
+      DESCRIPTION: 'Change the prefix of the bot.',
+      OPTIONS: {
+        PREFIX: {
+          NAME: 'new_prefix',
+          DESCRIPTION: 'The new prefix of the bot.'
+        }
+      },
+      EMBED: {
+        DESCRIPTION: 'Prefix changed to `{prefix:string}`.'
+      }
+    },
+    MAINTENANCE: {
+      DESCRIPTION: 'Set the maintenance mode of the bot.',
+      EMBED: {
+        DESCRIPTION: 'Maintenance mode set to `{state:string}`.'
+      }
+    },
+    STATS: {
+      DESCRIPTION: 'Get some stats about the bot.',
+      HEADERS: {
+        COMMANDS: 'Commands',
+        GUILDS: 'Guild',
+        ACTIVE_USERS: 'Active Users',
+        USERS: 'Users'
+      }
+    },
+    HELP: {
+      DESCRIPTION: 'Get global help about the bot and its commands',
+      EMBED: {
+        TITLE: 'Help panel',
+        CATEGORY_TITLE: '{category:string} Commands'
+      },
+      SELECT_MENU: {
+        TITLE: 'Select a category',
+        CATEGORY_DESCRIPTION: '{category:string} commands'
+      }
+    },
+    PING: {
+      DESCRIPTION: 'Pong!',
+      MESSAGE: '{member:string} Pong! The message round-trip took {time:number}ms.{heartbeat:string}'
+    }
+  }
+} as BaseTranslation);
